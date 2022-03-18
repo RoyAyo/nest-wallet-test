930a4133c971a47fe4de31494ad3d0c1f61048e6
RoyAyo <46534625+RoyAyo@users.noreply.github.com>
2022-03-18 14:43:21 +0100

    Update transactions.valitation.ts

--- src/transactions/transactions.valitation.ts
+++ src/transactions/transactions.valitation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const SendMoneyValidation = Joi.object().keys({
   reference: Joi.string().required(),
-  amount: Joi.number().required(),
+  amount: Joi.number().positive().required(),
   user: Joi.any().required(),
 });
 
